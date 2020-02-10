3952f76fdbf31a7558f575ffc2697f13c61dd9db
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-02-10 08:37:12 -0800

    Created all routing files

---
+++ routes.js
@@ -0,0 +1,14 @@
+module.exports = function(app){
+
+	//html routes
+	const application = require('./routes/application');
+	//api routes
+	const apiUsers = require('./routes/users');
+	const apiWorkouts = require('./routes/workouts');
+
+	// html routes
+	app.use('/', application);
+	// api routes
+	app.use('/api/users', apiUsers);
+	app.use('/api/workouts', apiWorkouts);
+}
